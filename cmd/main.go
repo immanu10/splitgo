5792357c35b32609d2564f412a30f1704e0c70d6
immanu10 <manojmanu8971117@gmail.com>
2025-03-13 22:44:59 +0530

    learn templating

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,67 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
-
-	"github.com/immanu10/splitgo/internal/handlers"
 )
 
+type Template struct{
+	templates *template.Template
+}
+
+func (t *Template) Render(w http.ResponseWriter, name string, data interface{}) error{
+	return t.templates.ExecuteTemplate(w,name,data)
+}
+
+func NewTemplate() *Template{
+	return &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+type LoginResponse struct{
+	Success bool
+	Username string
+}
+
+func newLoginResponse(success bool, username string) LoginResponse{
+	return LoginResponse{
+		Success: success,
+		Username: username,
+	}
+}
 
 func main(){
-	http.HandleFunc("/",handlers.RootHandler)	
-	http.HandleFunc("/login",handlers.LoginHandler)	
-	http.HandleFunc("/signup",handlers.SignUpHandler)	
+	t := NewTemplate()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t.Render(w,"index",nil)
+	})	
+	http.HandleFunc("/login",func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet{
+			t.Render(w,"login",newLoginResponse(false,""))	
+			return
+		}
+		if r.Method == http.MethodPost{
+			username := r.FormValue("username")
+			
+			t.Render(w,"login",newLoginResponse(true,username))	
+		}
+	})	
+	http.HandleFunc("/signup",func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet{
+			t.Render(w,"signup",nil)
+			return
+		}
+		if r.Method == http.MethodPost{
+
+		}
+	})	
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/",http.StripPrefix("/static/",fs))
-
-	http.ListenAndServe(":8080", nil)
+	
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	
 }
